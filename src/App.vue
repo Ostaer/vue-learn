67e6b2152d6230c1b14831f72a206a9b544a985d
liran <liran@daixiaomi.com>
2019-08-12 22:29:49 +0800

    4.3-webpack

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,20 @@ export default {
     return {
       firstName: 'Foo',
       lastName: 'Bar',
-      fullName: 'Foo Bar'
     }
   },
-  watch: {
-    firstName: function (val) {
-      this.fullName = val + ' ' + this.lastName
-    },
-    lastName: function (val) {
-      this.fullName = this.firstName + ' ' + val
+  computed: {
+    fullName: {
+      // getter
+      get: function () {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter
+      set: function (newValue) {
+        var names = newValue.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
     }
   }
 }
