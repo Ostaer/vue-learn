941056609dbf8dda679d4752c43e0c589c53a1b0
liran <liran@daixiaomi.com>
2019-08-12 22:41:37 +0800

    7.1-webpack

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
   <div id="app">
-    <div class="box" :class="{ 'active': isActive, 'text-danger': hasError }">
-      对象语法
-    </div>
-    <div class="box" :class="[isActive ? 'active' : '', hasError2 ? 'text-danger' : '' ]">
-      数组语法
-    </div>
-    <div class="box" :class="[{ 'active': isActive}, hasError2 ? 'text-danger' : '' ]">
-      数组语法嵌套对象语法
-    </div>
-    <div class="box active" :style="[{ 'background': bgColor}]">
-      修改style示例
-    </div>
+    <input v-model="message" placeholder="编辑我……">
+    <p>{{ message }}</p>
   </div>
 </template>
 
@@ -20,10 +10,7 @@ export default {
   name: 'App',
   data () {
     return {
-      isActive: true,
-      hasError: true,
-      hasError2: false,
-      bgColor: "blue"
+      message: 'Hello'
     }
   },
 }
@@ -37,18 +24,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-.box {
-  width: 100%;
-  height: 50px;
-  margin: 10px;
-  text-align: center;
-  background-color: black;
-}
-.active {
-  background-color: green;
-}
-.text-danger {
-  color: red;
 }
 </style>
