ab7529a14f823e33c9c970292a9bde3f3b55c224
liran <liran@daixiaomi.com>
2019-08-13 07:22:42 +0800

    8.8-webpack

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="app">
-    <blog-post :title="title"></blog-post>
+    <base-layout>
+      <template v-slot:header>
+        <h1>Here might be a page title</h1>
+      </template>
+
+      <p>A paragraph for the main content.</p>
+      <p>And another one.</p>
+
+      <template v-slot:footer>
+        <p>Here's some contact info</p>
+      </template>
+    </base-layout>
   </div>
 </template>
 
 <script>
-import blogPost from '@/components/blogPost'
+import baseLayout from '@/components/baseLayout'
 export default {
   name: 'App',
   components: {
-    blogPost
-  },
-  data () {
-    return {
-      title: '我是父组件标题'
-    }
+    baseLayout
   }
 }
 </script>
