01de77ab938a9e446cce8e88040b9e5e1c47fdbc
liran <liran@daixiaomi.com>
2019-08-12 23:36:33 +0800

    8.7-webpack

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div id="app">
-    <custom-input v-model="searchText"></custom-input>
+    <alert-box>
+      <template>
+        <span style="color: red;">这是插槽内容</span>
+      </template>
+    </alert-box>
   </div>
 </template>
 
 <script>
-import customInput from '@/components/customInput'
+import alertBox from '@/components/alertBox'
 export default {
   name: 'App',
   components: {
-    customInput
+    alertBox
   },
   data () {
     return {
-      searchText: "内容"
-    }
-  },
-  methods: {
-    onEnlargeText: function (enlargeAmount) {
-      this.postFontSize += enlargeAmount
     }
   }
 }
@@ -33,5 +31,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.demo-alert-box {
+  height: auto;
+  width: 100%;
+  border: 1px;
+  text-align: center;
+  -moz-box-shadow: 2px 2px 5px #333333;
+  -webkit-box-shadow: 2px 2px 5px #333333;
+  box-shadow: 2px 2px 5px #333333;
 }
 </style>
