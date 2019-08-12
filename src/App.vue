d7ef9bf24247d9c0849e5a5b78c2e14c8d0d3474
liran <liran@daixiaomi.com>
2019-08-12 22:34:30 +0800

    5.1-webpack

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    {{ fullName }}
+    <div class="box" :class="{ 'active': isActive, 'text-danger': hasError }">
+      对象语法
+    </div>
+    <div class="box" :class="[isActive ? 'active' : '', hasError2 ? 'text-danger' : '' ]">
+      数组语法
+    </div>
+    <div class="box" :class="[{ 'active': isActive}, hasError2 ? 'text-danger' : '' ]">
+      数组语法嵌套对象语法
+    </div>
+    <div class="box active" :style="[{ 'background': bgColor}]">
+      修改style示例
+    </div>
   </div>
 </template>
 
@@ -9,28 +20,16 @@ export default {
   name: 'App',
   data () {
     return {
-      firstName: 'Foo',
-      lastName: 'Bar',
+      isActive: true,
+      hasError: true,
+      hasError2: false,
+      bgColor: "blue"
     }
   },
-  computed: {
-    fullName: {
-      // getter
-      get: function () {
-        return this.firstName + ' ' + this.lastName
-      },
-      // setter
-      set: function (newValue) {
-        var names = newValue.split(' ')
-        this.firstName = names[0]
-        this.lastName = names[names.length - 1]
-      }
-    }
-  }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -38,5 +37,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.box {
+  width: 100%;
+  height: 50px;
+  margin: 10px;
+  text-align: center;
+  background-color: black;
+}
+.active {
+  background-color: green;
+}
+.text-danger {
+  color: red;
 }
 </style>
