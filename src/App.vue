2d99ccf6be3d3a97e16c989e5977ca689238e127
liran <liran@daixiaomi.com>
2019-08-13 07:44:33 +0800

    10.1-webpack

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,46 @@
 <template>
   <div id="app">
-    <base-layout>
-      <template v-slot:header>
-        <h1>Here might be a page title</h1>
-      </template>
+    <button
+      v-for="tab in tabs"
+      v-bind:key="tab"
+      v-bind:class="['tab-button', { active: currentTab === tab }]"
+      v-on:click="currentTab = tab"
+    >{{ tab }}
+    </button>
 
-      <p>A paragraph for the main content.</p>
-      <p>And another one.</p>
-
-      <template v-slot:footer>
-        <p>Here's some contact info</p>
-      </template>
-    </base-layout>
+    <keep-alive>
+      <component
+        v-bind:is="currentTabComponent"
+        class="tab"
+      ></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import baseLayout from '@/components/baseLayout'
+import tabPosts from '@/components/tabPosts'
+import tabArchive from '@/components/tabArchive'
 export default {
   name: 'App',
   components: {
-    baseLayout
+    tabPosts,
+    tabArchive
+  },
+  data () {
+    return {
+      currentTab: 'Posts',
+      tabs: ['Posts', 'Archive']
+    }
+  },
+  computed: {
+    currentTabComponent: function () {
+      return 'tab-' + this.currentTab.toLowerCase()
+    }
   }
 }
 </script>
 
-<style>
+<style lang="less">
 #app {
   -webkit-font-smoothing: antialiased;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -34,5 +49,64 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.tab-button {
+  padding: 6px 10px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background: #f0f0f0;
+  margin-bottom: -1px;
+  margin-right: -1px;
+}
+
+.tab-button:hover {
+  background: #e0e0e0;
+}
+
+.tab-button.active {
+  background: #e0e0e0;
+}
+
+.tab {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+.posts-tab {
+  display: flex;
+}
+
+.posts-sidebar {
+  max-width: 40vw;
+  margin: 0;
+  padding: 0 10px 0 0;
+  list-style-type: none;
+  border-right: 1px solid #ccc;
+}
+
+.posts-sidebar li {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.posts-sidebar li:hover {
+  background: #eee;
+}
+
+.posts-sidebar li.selected {
+  background: lightblue;
+}
+
+.selected-post-container {
+  padding-left: 10px;
+}
+
+.selected-post > :first-child {
+  margin-top: 0;
+  padding-top: 0;
 }
 </style>
