a783e5edacdd38154632c558c30d6b8405762426
liran <liran@daixiaomi.com>
2019-08-13 16:04:17 +0800

    14.3-webpack

--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,10 @@ const routes = [
     name: 'Home',
     // component: () => import('@/components/Home.vue') //ES6懒加载 会生成一个单独的home.[hash].js按需加载
     component: home
+  },
+  {
+    path: '/argu/:name',
+    component: () => import('@/components/argu.vue')
   }
 ]
 
